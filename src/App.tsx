2cf9c838c257af857082700b1238063ff6e0df51
bilcko <iubilcko@gmail.com>
2022-07-13 14:34:19 +0300

    FunctionComponent строгая типизация пропсов

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Card from "./components/Card";
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
     return (
         <div>
-            <Card width='200px' height='200px'>
+            <Card width='200px' height='200px' variant={CardVariant.outlined} onClick={(num) => console.log('click',num)}>
                 <button>
                     Кнопка
                 </button>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-
+import React, {FunctionComponent, useState} from 'react';
+export enum CardVariant {
+    outlined ='outlined',
+    primary = 'primapy'
+}
 interface CardProps{
     width: string;
     height: string;
-    children?: React.ReactNode
+    variant: CardVariant
+    children: React.ReactNode;
+    onClick: (num: number) => void;
 }
 
-const Card = ({width, children, height}: CardProps) => {
+const Card: FunctionComponent<CardProps> =
+    ({
+        width,
+        variant,
+        onClick, height,
+        children
+    }) => {
+    const [state, setState] = useState(0);
     return (
-        <div style={{width, height, border: '1px solid gray '}}>
+        <div style={{
+            width,
+            height,
+            border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+            background: variant === CardVariant.primary ? 'lightgrey' : '',
+        }}
+            onClick={() => onClick(state)}
+        >
             {children}
         </div>
     );
